3022870c70a8b62f601a2566cf8df6b1b0092163
ismael <ismael@ismael.com>
2024-05-30 18:28:09 -0300

    Configurando o react query

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,10 @@ import Processing from './screens/Processing'
 import Vaccinate from './screens/Vaccinate'
 import Brinco from './screens/brinco'
 // import Management from '@/screens/Management'
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query';
 
 import './css/bootstrap.css';
 import './css/noticia.css';
@@ -124,8 +128,12 @@ const router = createBrowserRouter([
     
 ]);
 
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={client}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </React.StrictMode>,
 )
